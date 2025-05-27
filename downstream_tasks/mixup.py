8c1453fff5e9fc0885bbaf5a91c2f505650201a5
lilypad <lily@creqavn.net>
2025-05-27 20:29:20 +0700

    implement onehot and rand bbox

--- downstream_tasks/mixup.py
+++ downstream_tasks/mixup.py
@@ -2,10 +2,37 @@ import random
 
 import numpy as np
 import torch
-from cutmix.cutmix import CutMix
-from cutmix.utils import onehot, rand_bbox
 from torch.utils.data.dataset import Dataset
 
+def onehot(size, target):
+    vec = torch.zeros(size, dtype=torch.float32)
+    vec[target] = 1.
+    return vec
+
+def rand_bbox(size, lam):
+    if len(size) == 4:
+        W = size[2]
+        H = size[3]
+    elif len(size) == 3:
+        W = size[1]
+        H = size[2]
+    else:
+        raise Exception
+
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
+
+    # uniform
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
 
 def calculate_confusion_matrix(pred, target):
     """Calculate confusion matrix according to the prediction and target.
