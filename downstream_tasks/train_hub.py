039e0a50a1544267ceb78d79baf925bcf2dc9400
hoclp <hoclp@xanhsm.com>
2025-06-24 11:03:35 +0700

    correctly import dataset

--- downstream_tasks/train_hub.py
+++ downstream_tasks/train_hub.py
@@ -14,8 +14,8 @@ from PIL.Image import Image
 from torchvision.models import ViT_B_16_Weights, resnet18, resnet50, vit_b_16
 from tqdm import tqdm
 
-from ..dataset.base import SyntheticDataset
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from dataset.base import SyntheticDataset
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 
